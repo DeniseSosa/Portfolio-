5ba388c4e2f422f9c62f1004d414806db5cb1776
unknown <rocio.sosa94@gmail.com>
2024-09-03 19:19:37 -0300

    cards

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,10 +1,10 @@
-import Card from "../Card/Card";
+import Cards from "../Cards/Cards";
 
 function Home() {
     return (
         <div className="bg-neutral-950 w-vh min-h-screen ">
             <h1 className="text-white">Hello Portfolio</h1>
-            <Card/>
+            <Cards/>
             
         </div>
     );
