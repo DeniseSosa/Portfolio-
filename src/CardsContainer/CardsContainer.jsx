60077b6a7aefef6d0b452b0f4616daaa6db3595a
unknown <rocio.sosa94@gmail.com>
2024-09-09 18:12:22 -0300

    technologies

--- src/CardsContainer/CardsContainer.jsx
+++ src/CardsContainer/CardsContainer.jsx
@@ -5,6 +5,8 @@ import Card from "../Card/Card";
 // eslint-disable-next-line react/prop-types
 function CardsContainer({projects}) {
     return (
+        <div>
+        <h2 className="text-white text-center text-3xl">Projects 🎉</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-6 justify-center m-5 p-2.5">
             {
                 // eslint-disable-next-line react/prop-types
@@ -19,6 +21,7 @@ function CardsContainer({projects}) {
                 
                 })
             }
+        </div>
         </div>
     );
   }
