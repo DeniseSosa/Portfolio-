53a4c9967e5e958ae4740f3bf00f1e25c8ca270f
unknown <rocio.sosa94@gmail.com>
2024-09-10 09:21:18 -0300

    hidden contact image

--- src/contact/Contact.jsx
+++ src/contact/Contact.jsx
@@ -3,35 +3,37 @@ function Contact() {
     <div className="">
       <h2 className="text-white text-center text-3xl">Contact 📩</h2>
       <div className="grid grid-cols-3 grid-rows-2 gap-4">
-        <div className="col-span-2">
-          <div className="flex flex-col justify-center">
-            <button className="text-white text-center text-3xl bg-fuchsia-900 p-4 m-1.5 rounded-md">
+
+        <div className=" col-span-3 lg:col-span-1 content-center">
+          <div className="flex flex-col justify-center items-center">
+            <button className="max-w-fit text-white text-center text-xl bg-fuchsia-900 p-4 m-1.5 rounded-md">
               Download Resume
             </button>
 
-            <button className="text-white text-center text-3xl bg-fuchsia-900  p-4 m-1.5 rounded-md">
+            <button className="max-w-fit text-white text-center text-xl bg-fuchsia-900  p-4 m-1.5 rounded-md">
               Descargar CV
             </button>
           </div>
         </div>
 
-        <div className="col-span-1 row-span-2">
+        <div className="col-span-2 row-span-2 hidden lg:visible">
           <img className="" src="/ro_mejorada.png"></img>
         </div>
 
-        <div className="col-span-2 row-span-1">
+        <div className="col-span-3 lg:col-span-1">
           <div className="flex justify-center">
             <img
-              className="size-1/6"
+              className="size-16 bg-white m-1.5"
               src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg"
             />
             <img
-              className="size-1/6"
+              className="size-16 bg-white m-1.5"
               src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linkedin/linkedin-original.svg"
             />
-            <img className="size-1/6" src="" alt="" />
+            <img className="size-16 bg-white m-1.5" src="/mail.svg" alt="" />
           </div>
         </div>
+
       </div>
     </div>
   );
