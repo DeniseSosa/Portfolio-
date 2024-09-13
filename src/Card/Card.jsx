02cdead57d15d5d32a236171e69fb42108527053
unknown <rocio.sosa94@gmail.com>
2024-09-13 19:32:40 -0300

    contact

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -5,7 +5,7 @@ function Card({ name, description, technologies }) {
   return (
     <div className="rounded-md m-2 p-5 border-2 border-fuchsia-800 hover:shadow-[0_10px_15px_rgb(192,38,211)] transition duration-300 hover:scale-105">
       <h2 className="text-white text-center text-4xl lg:text-5xl">{name}</h2>
-      <img src="/pixelcut-export.png"></img>
+      <img src="https://res.cloudinary.com/dcjdkojad/image/upload/v1726266624/Portfolio_images/Home_ixujdp.png"></img>
       <h3 className="text-white text-center text-2xl lg:text-3xl">
         Description: {description}
       </h3>
